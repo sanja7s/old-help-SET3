fca2e937135a8ac6d5dfc69a8f6fda2e05d6a58a
Sanja S <sanjascepanovic@gmail.com>
2012-12-20 14:51:26 +0200

    SET3: saving number and frequency of user calls into files and filtering users by some thresholds

--- D4D_SET3/__main__.py
+++ D4D_SET3/__main__.py
@@ -6,57 +6,36 @@ Created on Nov 21, 2012
 
 import logging
 import traceback
-from multiprocessing import Pool
-#from read_in import data as rd
-from read_in import seq_mining_data as rd
-#from analyze import night_homes as a
-from analyze import seq_mining_weeks as a
+import numpy as n
 
-#from visualize import graph_statistics as gs
+from multiprocessing import Pool
+from read_in import data as rd
+from analyze import fq_calling_users as a
 
 _log = logging.getLogger(__name__)
 
 def main():
 
     logging.basicConfig(level=logging.INFO, format='%(name)s: %(levelname)-8s %(message)s')
-    
-    # Build a pool of 8 processes
-    #pool = Pool(processes=8,)
-    
-    #c = ['A','B','C','D','E', 'F', 'G','H','I', 'J']
-    #c = 'C'
-    #data = pool.map(rd.read_in_file, c)
-    #res = pool.map(nh.find_home,(data, c))
-    
-    #data = rd.read_in_file(c)
-    #C = ['A','B','C','D','E', 'F', 'G','H','I', 'J']
         
-    C = ['A']
-    home_subprefs = [7]
-    #data = {}
+    C = ['A','B','C','D','E','F','G','H','I','J']
+    data = n.zeros(500001, dtype=n.int)
     
-    #for  c in C:
-        #data = rd.read_in_file(c, data)
-        
-    data = rd.read_in_file(C, home_subprefs)
+    for  c in C:
+        data = rd.read_in_whole_file(c, data)
     
-    
-    #_log.info("Data loaded.")
+    _log.info("Data loaded.")
+    fq_data = a.save_usr_calling_fq(data)
     while True:
         raw_input("Press enter to start a process cycle:\n")
         try:
-            #reload(rd)
             reload(a)
         except NameError:
             _log.error("Could not reload the module.")
         try:
             # THIS THE FUNCTION YOU ARE TESTING
-            #a.find_home(pd.find_usr_call_fq(data))  
-            #a.fq_places(by_night = True, save_to_file = True)
-#            home = {}
-#            home = a.find_home(data, home, c)
-#            a.print_home('ALL', home)
-            a.freq_seq(data)
+            fq = 0.2
+            a.filter_fq_callers(fq_data, fq)
             
         except Exception as e:
             _log.error("Caught exception from the process\n%s\n%s" % (e, traceback.format_exc()))
